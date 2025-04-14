db318e97e081fff99ef594a06e3b853dc9ead4ba
Luis Carranza <lcarranzal1@upao.edu.pe>
2025-04-13 22:01:10 -0500

    configuracion de cambio de puerto

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "docker_container" "container_server_web" {
   name = "server_web"
   image = "nginx:latest"
   ports {
-    external = 6000
+    external = 4000
     internal = 80
 }
 }   
